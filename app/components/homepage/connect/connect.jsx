0e917b12df8e56e9066537219876feacf09b48d0
shafaet-j <lbshaft01@gmail.com>
2024-03-03 22:27:45 +0600

    connect section done

--- app/components/homepage/connect/connect.jsx
+++ app/components/homepage/connect/connect.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
 
-const Work = () => {
+const Connect = () => {
   return (
     <section className=" flex items-center py-10">
       <div className="max-w-[1140px] mx-auto">
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5  mb-10 items-center">
-          <div>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-28  mb-10 items-center">
+          <div className=" order-2">
             <h2 className=" text-blackis font-bold text-5xl mb-4">
-              Work together, wherever you work
+              Connect your tools, close your tabs
             </h2>
             <p className=" text-grey/50 mb-10">
-              In the office, remote, or a mix of the two, with Miro, your team
-              can connect, collaborate, and co-create in one space no matter
-              where you are.
+              Whether you want to edit your Google Docs, resolve Jira issues, or
+              collaborate over Zoom, Miro has 100+ integrations with tools you
+              already use and love.
             </p>
             <button className=" flex items-center gap-2  text-blue text-left ">
               <span className=" border-b border-blue pb-1"> Learn more</span>
               <IoIosArrowRoundForward />
             </button>
           </div>
-          <div>
-            <img src="./work.png" alt="" />
+          <div className=" order-1">
+            <img src="./connect.png" alt="" />
           </div>
         </div>
       </div>
@@ -29,4 +29,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default Connect;
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Banner from "./components/homepage/banner/banner";
 import Brands from "./components/homepage/brands/brands";
 import Colaborate from "./components/homepage/collaborate/colaborate";
 import Work from "./components/homepage/work/work";
+import Connect from "./components/homepage/connect/connect";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Brands />
       <Colaborate />
       <Work />
+      <Connect />
     </div>
   );
 }
